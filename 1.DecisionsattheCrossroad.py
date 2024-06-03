30107f43d3a2cd106d83663627e9f0ec80a2f08b
James Z <wuab@yahoo.com>
2024-06-03 15:27:15 -0700

    Update 1.DecisionsattheCrossroad.py

--- 1.DecisionsattheCrossroad.py
+++ 1.DecisionsattheCrossroad.py
@@ -1,4 +1,4 @@
-1.DecisionsattheCrossroad
+#1.DecisionsattheCrossroad
 
 # Task 1: Code Correction You are provided with a Python script that uses conditional statements to tell if a number is positive, negative, or zero, but it has some errors. Identify and fix them.
 # Buggy Code:
